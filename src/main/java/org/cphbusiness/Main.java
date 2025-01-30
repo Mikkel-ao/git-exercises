2ccf152b76f1da14f810fb77f6c3b92fad1698e7
Mikkel-ao <mikkel.ao.dev@gmail.com>
2025-01-30 11:59:31 +0100

    fizzBuzz added

--- src/main/java/org/cphbusiness/Main.java
+++ src/main/java/org/cphbusiness/Main.java
@@ -2,6 +2,20 @@ package org.cphbusiness;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        fizzBuzz();
+    }
+
+    public static void fizzBuzz() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+        }
     }
 }
